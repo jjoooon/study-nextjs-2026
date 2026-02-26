cf1a3cb13956a6c9fe10e2c13ec7399b96b57a72
윤준모 <yjm5848@naver.com>
2026-02-26 14:51:51 +0900

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -68,7 +68,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" suppressHydrationWarning>
       <body>
         <Providers>
           <AuthGuard>{children}</AuthGuard>
