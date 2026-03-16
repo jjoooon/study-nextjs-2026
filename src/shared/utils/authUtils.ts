97edebd71cad31a91af6dc11acb92a482a2d5280
윤준모 <yjm5848@naver.com>
2026-03-16 13:29:07 +0900

    .

--- src/shared/utils/authUtils.ts
+++ src/shared/utils/authUtils.ts
@@ -7,19 +7,11 @@ const logger = log.getLogger('AuthUtils');
 // TODO: @YunJunmo 임시로 authSlice의 사번 사용; 추후 ncrm 연동 필요
 
 /**
- * 인증 헤더 정보 타입
- *
- * @description
- * 시스템 간 통신에 필요한 인증 정보를 정의합니다.
- * 현재는 임시로 하드코딩된 값을 사용하지만,
- * 추후 세션 스토리지나 외부 인증 시스템과 연동할 예정입니다.
+ * 헤더 타입
  */
 interface Header {
-  /** 트랜잭션 코드 */
   pfmTxCode: string;
-  /** 글로벌 번호 */
   pfmGlobalNo: string;
-  /** 직원 번호 */
   pfmStfno: string;
 }
 
