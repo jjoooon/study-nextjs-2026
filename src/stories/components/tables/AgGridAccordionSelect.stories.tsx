7733d010abab821864a50dd68755ad743c26e6d4
asever77 <johyunmin77@gmail.com>
2026-03-19 15:26:39 +0900

    정렬 아이콘 크기 재수정

--- src/stories/components/tables/AgGridAccordionSelect.stories.tsx
+++ src/stories/components/tables/AgGridAccordionSelect.stories.tsx
@@ -69,7 +69,7 @@ const columnDefsString: ColDef<StringDataType>[] = [
 ];
 
 const meta: Meta<typeof AgGridReact<NumberDataType>> = {
-  title: 'Components/Tables/AgGrid/CellEditor Input',
+  title: 'Components/Tables/AgGrid/CellEditor Select',
   component: AgGridReact,
   tags: ['autodocs'],
   parameters: {
