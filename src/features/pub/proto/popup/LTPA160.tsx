ade88f126c31c2c8b8830c188ded1819cee1bbf5
허승하 <hide5075@gmail.com>
2026-03-31 15:54:40 +0900

    인수지침 작업중

--- src/features/pub/proto/popup/LTPA160.tsx
+++ src/features/pub/proto/popup/LTPA160.tsx
@@ -206,8 +206,21 @@ export const LTPA160P = ({ open, onOpenChange }: LTPA160PProps) => {
           </DialogTitle>
         </DialogHeader>
         <DialogSection className='grid-rows-[auto_1fr]'>
+          <Grow placement='bwc' className="w-full" variant={'box-round'}>
+            <FormTable variant={'head'} lineTop={false} caption="보험정보" cols={['w-[14rem] min-w-[14rem]', 'w-[20rem] min-w-[20rem]', 'w-[14rem] min-w-[14rem]', 'w-auto']}>
+              <FormRow>
+                <FormCell title={'조회구분'}>
+                  <Input aria-label="" width={'10rem'} value={'text'} readOnly />
+                </FormCell>
+                <FormCell title={'피보험자정보'}>
+                  <Grow>
+                    <Input aria-label="" width={'20rem'} value={'무배당 1등 엄마의 똑똑한 자녀보힘 1404'} readOnly />
+                  </Grow>
+                </FormCell>
+              </FormRow>
+            </FormTable>
+          </Grow>
 
-          
           <TableFold>
             <TableFoldHead title="피보험자의 위험정보(고객정보)">
             </TableFoldHead>
