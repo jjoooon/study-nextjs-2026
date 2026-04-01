a9af4de21f0f4321576b735c77846358e78f56ab
asever77 <johyunmin77@gmail.com>
2026-04-01 13:29:56 +0900

    수정

--- src/stories/popup/LTPA170.stories.tsx
+++ src/stories/popup/LTPA170.stories.tsx
@@ -14,7 +14,7 @@ export const Default = () => {
   const [open, setOpen] = React.useState(true);
   return (
     <LayoutDoc>
-      <div className='flex w-full h-screen items-center justify-center'>
+      <div className='flex w-full h-screen items-center justify-center max-w-[118rem] outline outline-1 outline-[red] -outline-offset-2 mx-auto'>
         <Button variant={'contained'} onClick={() => setOpen(true)}>다이얼로그 열기</Button>
       </div>
 
