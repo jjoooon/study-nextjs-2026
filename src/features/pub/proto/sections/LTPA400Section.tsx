4e84f5fb1a2891c492f0081221b15f0029b2a138
허승하 <hide5075@gmail.com>
2026-04-02 11:26:55 +0900

    가입설계 - 장기보험_가입설계요청

--- src/features/pub/proto/sections/LTPA400Section.tsx
+++ src/features/pub/proto/sections/LTPA400Section.tsx
@@ -6,16 +6,20 @@ import { LayoutTemplate } from '@layout/LayoutTemplate';
 import PageID from '@features/PageID';
 
 // data
-import { LTPA400Data } from '../data/LTPA400Data';
 import { LTPA400Main } from '../components/LTPA400Main';
 
+const data = {
+  pageID: {
+    pageName: '장기보험_가입설계요청',
+    pageId: 'LTPA400',
+  },
+}
 
 export default function LTPA400Section() {
   return (
     <LayoutTemplate
       // LayoutHead
-      pageID={<PageID data={LTPA400Data.head.pageID} />}
-      // LayoutBody: main
+      pageID={<PageID data={data.pageID} />}
       mainBody={<LTPA400Main />}
     />
   );
