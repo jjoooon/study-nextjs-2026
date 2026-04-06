b6a0ddb09cfee95894ee8b9e3086577e4bcade88
new-Niel <endlessiris@netive.co.kr>
2026-04-06 16:58:15 +0900

    Merge

--- src/stories/IAListWithPreview.tsx
+++ src/stories/IAListWithPreview.tsx
@@ -147,7 +147,7 @@ export function IAListWithPreview() {
     'LTPZ010', 'LTPZ011', 'LTPZ017', 'LTPZ020', 'LTPA160', 'LTPA904', 'LTPZ999', 'LTPZ998', 'LTPZ997', 'LTPZ996', 'LTPA170', 'LTPA904', 'LTPA390', 'LTPA430', 'LTPA070', 'LTPA010', 'LTPZ041', 'LTPZ042', 'LTPZ038', 'LTPZ039', 'LTPA400', 'LTPA210', 'LTPA200', 'LTPA190', 'LTPZ001', 'LTPZ040', 'LTPZ043', 'LTPZ046', 'LTPZ047', 'LTPA030', 'LTPA360', 'LTPZ049', 'LTPZ050', 'LTPZ051', 'LTPZ002', 'LTPZ052', 'LTPZ053', 'LTPZ057', 'LTPZ009', 'LTPA401', 'LTPA301', 'LTPA303', 'LTPZ048', 'LTPZ045', 'LTRZ085', 'LTPA300', 'LTPZ994', 'LTPZ995'
   ];
   const workList_h: string[] = [];
-  const workList_k: string[] = ['LTRZ086'];
+  const workList_k: string[] = ['LTPZ085','LTPZ086'];
   const workList_j: string[] = [];
 
   const workList = [...workList_prev, ...workList_h, ...workList_k, ...workList_j];
