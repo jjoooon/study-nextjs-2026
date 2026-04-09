c678e7fbf6d5d051615001d912f0f29ec443ae26
new-Niel <endlessiris@netive.co.kr>
2026-04-09 13:42:32 +0900

    LTpa020 진행중

---
+++ src/stories/page/LTPA020.stories.tsx
@@ -0,0 +1,13 @@
+import LTPA020 from '@/app/pub/pages/LTPA020';
+import { LayoutDoc } from '@layout/BaseLayout';
+
+export default {
+  title: 'page/LTPA020',
+  component: LTPA020,
+};
+
+export const Default = () => (
+  <LayoutDoc>
+    <LTPA020 />
+  </LayoutDoc>
+);
