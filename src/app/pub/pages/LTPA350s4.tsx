f8c273ead87f9a0df4e20b182819e84a3067fcff
허승하 <hide5075@gmail.com>
2026-04-09 19:31:22 +0900

    설계진행 > 추가사항 페이지 생성

---
+++ src/app/pub/pages/LTPA350s4.tsx
@@ -0,0 +1,5 @@
+import Ltpa350s4Section from '@/features/pub/sections/Ltpa350s4Section';
+
+export default function Page() {
+  return <Ltpa350s4Section />;
+}
---
+++ src/stories/page/LTPA350s4.stories.tsx
@@ -0,0 +1,13 @@
+import LTPA350s4 from '@/app/pub/pages/LTPA350s4';
+import { LayoutDoc } from '@layout/BaseLayout';
+
+export default {
+  title: 'page/LTPA350s4',
+  component: LTPA350s4,
+};
+
+export const Default = () => (
+  <LayoutDoc>
+    <LTPA350s4 />
+  </LayoutDoc>
+);
