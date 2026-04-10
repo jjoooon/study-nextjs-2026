21a4b85177235fd3e636267b4830ab6cc2d17942
new-Niel <endlessiris@netive.co.kr>
2026-04-10 17:50:12 +0900

    Z019

--- src/features/pub/components/popups/Ltpz019.tsx
+++ src/features/pub/components/popups/Ltpz019.tsx
@@ -1,11 +1,11 @@
 'use client';
 // 권오택
-import type { ColDef } from 'ag-grid-community';
+import type { ColDef, ICellRendererParams } from 'ag-grid-community';
 import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
 import { AgGridReact } from 'ag-grid-react';
-import { AgGridEmptyComponent, useToggleTopRows } from '@/shared/components/agGridUtils';
+import { AgGridEmptyComponent, createTooltipValueGetter, useToggleTopRows } from '@/shared/components/agGridUtils';
 import type { PopupBaseProps } from '@/shared/types/uiTypes';
-import { Gcol, Grow, Typo } from '@atoms';
+import { Divider, Gcol, Grow, Typo } from '@atoms';
 import { DialogBottomInfo } from '@common/DialogBottomInfo';
 import { Button } from '@uiux/Button';
 import {
@@ -22,183 +22,331 @@ import { FormCell, FormRow, FormTable } from '@common/FormTable';
 import { Badge } from '@uiux/Badge';
 import { RadioGroup, RadioGroupItem } from '@uiux/RadioGroup';
 import { TableFold, TableFoldBody, TableFoldHead } from '@common/TableFold';
+import { Checkbox } from '@/shared/components/uiux/Checkbox';
+import { useCallback, useState } from 'react';
+import { ResetIcon, SearchIcon } from '@/shared/components/icons';
+import { Input } from '@/shared/components/uiux/Input';
+import { TabPager } from '@/shared/components/common/TabPager';
+import { useTabs } from '@/shared/hooks/useTabs';
 
 ModuleRegistry.registerModules([AllCommunityModule]);
 
 export const Ltpz019 = ({ open, onOpenChange }: PopupBaseProps) => {
+
+  const [checkedMap, setCheckedMap] = useState({ selected: true, unselected: false });
+  const [showProductNameTooltip, setShowProductNameTooltip] = useState(false);
+  const [gridKey, setGridKey] = useState(0);
+  const [coverageName, setCoverageName] = useState('');
+  const handleActionButtonClick = useCallback(() => {}, []);
+  const handleCheckedChange = (key: string) => (checked: boolean | 'indeterminate') => {
+    setCheckedMap((map) => ({ ...map, [key]: !!checked }));
+  };
+
+  const productNameHeader = useCallback(() => {
+    const handleTooltipCheck = (checked: boolean | 'indeterminate') => {
+      setShowProductNameTooltip(!!checked);
+      if (!checked) setGridKey((key) => key + 1);
+    };
+    return (
+      <Grow className="w-full px-[0.6rem]" placement={'sc'} gap={4}>
+        <Grow>
+          <Input
+            aria-label="상품명"
+            placeholder="상품명 입력"
+            type="text"
+            width={'full'}
+            size={'sm'}
+            clear={true}
+            value={coverageName}
+            onChange={(e) => setCoverageName(e.target.value)}
+          />
+          <Button aria-label="상품명 검색" variant={'outlined'} color={'gray-light'} only={'icon'} size={'md'}>
+            <SearchIcon color={'var(--color-primary-50)'} />
+          </Button>
+          <Button
+            aria-label="상품명 초기화"
+            variant={'outlined'}
+            color={'gray-light'}
+            only={'icon'}
+            size={'md'}
+            onClick={() => setCoverageName('')}
+          >
+            <ResetIcon color={'var(--color-primary-50)'} />
+          </Button>
+        </Grow>
+        <Grow placement={'sc'}>
+          <Checkbox size={'md'} checked={showProductNameTooltip} onCheckedChange={handleTooltipCheck}>
+            상품명 말풍선
+          </Checkbox>
+        </Grow>
+      </Grow>
+    );
+  }, [checkedMap, coverageName, showProductNameTooltip]);
+
+  const titleRenderer = useCallback((params: ICellRendererParams<DummyDataType>) => {
+    return <p className="truncate w-full pl-1.5">{params.data?.field2 ?? ''}</p>;
+  }, []);
+
+
   type DummyDataType = {
     id: number;
     field1: string | number;
     field2: string | number;
-    field3: string | number;
-    field4: string | number;
-    field5: string | number;
-    field6: boolean;
   };
 
   const dummyData: DummyDataType[] = [
     {
       id: 1,
-      field1: '2026-02-24',
-      field2: 80939583,
-      field3: '홍길순1',
-      field4: '여의도 GA 지점',
-      field5: '메모 테스트 글입니다.',
-      field6: false,
+      field1: '종합건강22',
+      field2: '{LA1312312}한화 311 간편건강보험(연만기 경신쳥)',
     },
     {
       id: 2,
-      field1: '2026-02-24',
-      field2: 80939583,
-      field3: '홍길순2',
-      field4: '여의도 GA 지점',
-      field5: '메모 테스트 글입니다.',
-      field6: false,
+      field1: '종합건강',
+      field2: '{LA1312312}한화 311 간편건강보험(연만기 경신쳥)',
     },
     {
       id: 3,
-      field1: '2026-02-24',
-      field2: 80939583,
-      field3: '홍길순3',
-      field4: '여의도 GA 지점',
-      field5: '메모 테스트 글입니다.',
-      field6: false,
+      field1: '종합건강',
+      field2: '{LA1312312}한화 311 간편건강보험(연만기 경신쳥)',
+    },
+    {
+      id: 4,
+      field1: '종합건강',
+      field2: '{LA1312312}한화 311 간편건강보험(연만기 경신쳥)',
+    },
+    {
+      id: 5,
+      field1: '종합건강',
+      field2: '{LA1312312}한화 311 간편건강보험(연만기 경신쳥)',
+    },
+    {
+      id: 6,
+      field1: '종합건강',
+      field2: '{LA1312312}한화 311 간편건강보험(연만기 경신쳥)',
+    },
+    {
+      id: 7,
+      field1: '종합건강',
+      field2: '{LA1312312}한화 311 간편건강보험(연만기 경신쳥)',
+    },
+    {
+      id: 8,
+      field1: '종합건강',
+      field2: '{LA1312312}한화 311 간편건강보험(연만기 경신쳥)',
+    },
+    {
+      id: 9,
+      field1: '종합건강',
+      field2: '{LA1312312}한화 311 간편건강보험(연만기 경신쳥)',
+    },
+    {
+      id: 10,
+      field1: '종합건강',
+      field2: '{LA1312312}한화 311 간편건강보험(연만기 경신쳥)',
+    },
+    {
+      id: 11,
+      field1: '종합건강',
+      field2: '{LA1312312}한화 311 간편건강보험(연만기 경신쳥)',
+    },
+    {
+      id: 12,
+      field1: '종합건강',
+      field2: '{LA1312312}한화 311 간편건강보험(연만기 경신쳥)',
+    },
+    {
+      id: 13,
+      field1: '종합건강',
+      field2: '{LA1312312}한화 311 간편건강보험(연만기 경신쳥)',
+    },
+    {
+      id: 14,
+      field1: '종합건강',
+      field2: '{LA1312312}한화 311 간편건강보험(연만기 경신쳥)',
+    },
+    {
+      id: 20,
+      field1: '종합건강',
+      field2: '{LA1312312}한화 311 간편건강보험(연만기 경신쳥)',
     },
   ];
 
   const columnDefs: ColDef<DummyDataType>[] = [
     {
-      headerName: '입력일', 
+      headerName: '상품분류', 
       field: 'field1',
+      cellClass: 'text-center',
       width: 100,
     },
     {
-      headerName: '입력자사번',
-      field: 'field2',
-      width: 100,
+      headerName: '상품명',
+      flex: 1,
+      cellClass: 'text-left p-0!',
+      sortable: false,
+      filter: false,
+      autoHeight: true,
+      suppressMovable: true, // 이동 방지
+      lockPinned: true, // 고정 열에서 제외 방지
+      tooltipValueGetter: createTooltipValueGetter<DummyDataType>({
+        label: '상품명',
+        field: 'field2',
+      }),
+      headerComponent: productNameHeader,
+      cellRenderer: titleRenderer,
     },
-    {
-      headerName: '입력자명',
-      field: 'field3',
-      width: 100,
-    },
+    
   ];
+
+  const designCellRenderer = (params: ICellRendererParams<DummyDataType>) => {
+    return (
+      <Grow className="h-full w-full">
+        <Grow className="border-r border-[#ddddde] h-full aspect-auto w-[3rem] flex items-center justify-center shrink-0">
+          {params.data?.field1}
+        </Grow>
+        <Grow className="flex-1 justify-start">
+          {params.data?.field2}
+        </Grow>
+      </Grow>
+    );
+  };
 
   type DummyDataType2 = {
     id: number;
     field1: string | number;
     field2: string | number;
-    field3: string | number;
-    field4: string | number;
-    field5: string | number;
-    field6: boolean;
   };
 
   const dummyData2: DummyDataType2[] = [
     {
       id: 1,
-      field1: '2026-02-24',
-      field2: 80939583,
-      field3: '홍길순1',
-      field4: '여의도 GA 지점',
-      field5: '메모 테스트 글입니다.',
-      field6: false,
+      field1: '1종',
+      field2: '납입면제 강화형, 기본형(할증운영상품)',
     },
     {
       id: 2,
-      field1: '2026-02-24',
-      field2: 80939583,
-      field3: '홍길순2',
-      field4: '여의도 GA 지점',
-      field5: '메모 테스트 글입니다.',
-      field6: false,
+      field1: '2종',
+      field2: '납입면제 강화형, 기본형(할증운영상품)',
     },
     {
       id: 3,
-      field1: '2026-02-24',
-      field2: 80939583,
-      field3: '홍길순3',
-      field4: '여의도 GA 지점',
-      field5: '메모 테스트 글입니다.',
-      field6: false,
+      field1: '3종',
+      field2: '납입면제 강화형, 기본형(할증운영상품)',
+    },
+    {
+      id: 4,
+      field1: '4종',
+      field2: '납입면제 강화형, 기본형(할증운영상품)',
+    },
+    {
+      id: 5,
+      field1: '4종',
+      field2: '납입면제 강화형, 기본형(할증운영상품)',
+    },
+    {
+      id: 6,
+      field1: '4종',
+      field2: '납입면제 강화형, 기본형(할증운영상품)',
+    },
+    {
+      id: 7,
+      field1: '4종',
+      field2: '납입면제 강화형, 기본형(할증운영상품)',
+    },
+    {
+      id: 8,
+      field1: '4종',
+      field2: '납입면제 강화형, 기본형(할증운영상품)',
+    },
+    {
+      id: 9,
+      field1: '4종',
+      field2: '납입면제 강화형, 기본형(할증운영상품)',
     },
   ];
 
   const columnDefs2: ColDef<DummyDataType2>[] = [
     {
-      headerName: '상', 
+      headerName: '종구분', 
       field: 'field1',
-      width: 100,
-    },
-    {
-      headerName: '입력자사번',
-      field: 'field2',
-      width: 100,
-    },
-    {
-      headerName: '입력자명',
-      field: 'field3',
-      width: 100,
+      flex: 1,
+      cellRenderer: designCellRenderer,  
     },
   ];
 
   type DummyDataType3 = {
     id: number;
     field1: string | number;
-    field2: string | number;
-    field3: string | number;
-    field4: string | number;
-    field5: string | number;
-    field6: boolean;
   };
 
   const dummyData3: DummyDataType3[] = [
     {
       id: 1,
-      field1: '2026-02-24',
-      field2: 80939583,
-      field3: '홍길순1',
-      field4: '여의도 GA 지점',
-      field5: '메모 테스트 글입니다.',
-      field6: false,
+      field1: '올인원플랜(15~40세)',
     },
     {
       id: 2,
-      field1: '2026-02-24',
-      field2: 80939583,
-      field3: '홍길순2',
-      field4: '여의도 GA 지점',
-      field5: '메모 테스트 글입니다.',
-      field6: false,
+      field1: '비대면진단심사플랜(15~40세)',
     },
     {
       id: 3,
-      field1: '2026-02-24',
-      field2: 80939583,
-      field3: '홍길순3',
-      field4: '여의도 GA 지점',
-      field5: '메모 테스트 글입니다.',
-      field6: false,
+      field1: '비대면진단심사플랜(15~40세)',
+    },
+    {
+      id: 4,
+      field1: '올인원플랜(15~40세)',
+    },
+    {
+      id: 5,
+      field1: '올인원플랜(15~40세)',
+    },
+    {
+      id: 6,
+      field1: '올인원플랜(15~40세)',
+    },
+    {
+      id: 7,
+      field1: '올인원플랜(15~40세)',
+    },
+    {
+      id: 8,
+      field1: '올인원플랜(15~40세)',
     },
   ];
 
   const columnDefs3: ColDef<DummyDataType3>[] = [
     {
-      headerName: '입력일', 
+      headerName: '플랜면', 
       field: 'field1',
-      width: 100,
-    },
-    {
-      headerName: '입력자사번',
-      field: 'field2',
-      width: 100,
-    },
-    {
-      headerName: '입력자명',
-      field: 'field3',
-      width: 100,
+      flex: 1,
     },
   ];
+
+  type Ltpz032TabType = {
+    name: string;
+    value: string;
+    label: string;
+  };
+
+  const DATA_TABS: Ltpz032TabType[] = [
+    {
+      name: '회사플랜',
+      value: 'tab1',
+      label: '회사플랜(6)',
+    },
+    {
+      name: '기관플랜',
+      value: 'tab2',
+      label: '기관플랜(6)',
+    },
+    {
+      name: '나만의플랜',
+      value: 'tab3',
+      label: '나만의플랜(6)',
+    },
+  ];
+
+  const { tabs, active, setActive, handleRemove } = useTabs(DATA_TABS);
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
@@ -227,7 +375,12 @@ export const Ltpz019 = ({ open, onOpenChange }: PopupBaseProps) => {
               </FormRow>
             </FormTable>
           </Grow>
-          <Gcol placement={'ss'} className="w-full" gap={5}>       
+          <Gcol placement={'ss'} className="w-full" gap={5}>
+             <Gcol placement={'ss'} className="w-full">
+              <Typo variant={'body-lg'} weight={'bold'} className='flex items-center'>
+                간편설계를 생성할 상품을 선택해주세요. 
+              </Typo>
+            </Gcol> 
             <Gcol placement={'ss'} className="w-full">
               <Typo variant={'body-lg'} weight={'bold'} className='flex items-center gap-[0.6rem]'>
                 <Badge color="secondary" size="md" variant="contained" className='w-[1.8rem] h-[1.8rem]'>1</Badge>
@@ -269,58 +422,82 @@ export const Ltpz019 = ({ open, onOpenChange }: PopupBaseProps) => {
 
 
               <Grow placement={'ss'} className="w-full gap-6">
-                <Grow className='w-full'>
+                <Grow className='w-full' placement='ss' gap={5}>
                   <TableFold>
                     <TableFoldHead title="상품정보"></TableFoldHead>
-                    <TableFoldBody>
-                      <div className="ag-theme-alpine">
+                    <TableFoldBody className="w-full">
+                      <div className="ag-theme-alpine w-full h-160!">
                         <AgGridReact<DummyDataType>
                           getRowId={(params) => String(params.data.id)}
                           noRowsOverlayComponent={AgGridEmptyComponent}
                           rowData={dummyData}
                           columnDefs={columnDefs}
                           defaultColDef={{
-                            sortable: true,
-                            resizable: true,
+                            sortable: false,
+                            resizable: false,
                           }}
-                          domLayout="autoHeight"
+                          headerHeight={30}
+                          rowHeight={30}
+                          domLayout="normal"
                         />
                       </div>
                     </TableFoldBody>
                   </TableFold>
                   <TableFold>
-                    <TableFoldHead title="종 정보"></TableFoldHead>
+                    <TableFoldHead title="(LA0203413)한화 시그니처 여성 건강"></TableFoldHead>
                     <TableFoldBody>
-                      <Gcol className='w-full'>
-                        <div className="ag-theme-alpine">
-                          <AgGridReact<DummyDataType2>
-                            getRowId={(params) => String(params.data.id)}
-                            noRowsOverlayComponent={AgGridEmptyComponent}
-                            rowData={dummyData2}
-                            columnDefs={columnDefs2}
-                            defaultColDef={{
-                              sortable: true,
-                              resizable: true,
-                            }}
-                            domLayout="autoHeight"
-                          />
-                        </div>
-                      </Gcol>
-                      <Gcol className='w-full'>
-                         <div className="ag-theme-alpine">
-                          <AgGridReact<DummyDataType3>
-                            getRowId={(params) => String(params.data.id)}
-                            noRowsOverlayComponent={AgGridEmptyComponent}
-                            rowData={dummyData3}
-                            columnDefs={columnDefs3}
-                            defaultColDef={{
-                              sortable: true,
-                              resizable: true,
-                            }}
-                            domLayout="autoHeight"
-                          />
-                        </div>   
-                      </Gcol>
+                      <Gcol className='w-full' gap={5}>
+                        <Gcol className='w-full'>
+                            <div className="ag-theme-alpine w-full h-70!">
+                            <AgGridReact<DummyDataType2>
+                              getRowId={(params) => String(params.data.id)}
+                              noRowsOverlayComponent={AgGridEmptyComponent}
+                              rowData={dummyData2}
+                              columnDefs={columnDefs2}
+                              defaultColDef={{
+                                sortable: false,
+                                resizable: false,
+                              }}
+                              headerHeight={30}
+                              rowHeight={30}
+                              domLayout="normal"
+                            />
+                          </div>
+                        </Gcol>
+                        <Gcol className='w-full'>
+                           <TabPager
+                              data={tabs}
+                              active={active}
+                              setActive={setActive}
+                              removable={false}
+                              onRemove={handleRemove}
+                              visibleCount={4}
+                              variant="default"
+                              hasTableBelow={true}
+                              error={false}
+                              errorMsg="에러 메시지 예시"
+                              getValue={(tab) => String(tab.value)}
+                              renderTab={(tab) => <span>{tab.label}</span>}
+                              renderDropdownItem={false}
+                            >
+                            <div className="ag-theme-alpine w-full h-70! ag-border-t">
+                              <AgGridReact<DummyDataType3>
+                                getRowId={(params) => String(params.data.id)}
+                                noRowsOverlayComponent={AgGridEmptyComponent}
+                                rowData={dummyData3}
+                                columnDefs={columnDefs3}
+                                defaultColDef={{
+                                  sortable: false,
+                                  resizable: false,
+                                }}
+                                headerHeight={30}
+                                rowHeight={30}
+                                domLayout="normal"
+                              />
+                            </div>
+                          </TabPager>
+                        </Gcol>
+                      </Gcol>  
                     </TableFoldBody>
                   </TableFold>
                 </Grow>
@@ -349,3 +526,4 @@ export const Ltpz019 = ({ open, onOpenChange }: PopupBaseProps) => {
     </Dialog>
   );
 };
+
