695fa1f31872d1d02d88b5181cdf35c1fe834e3a
new-Niel <endlessiris@netive.co.kr>
2026-04-10 20:53:56 +0900

    004

---
+++ src/features/pub/components/popups/Ltpz004.tsx
@@ -0,0 +1,239 @@
+'use client';
+
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
+import type { ColDef } from 'ag-grid-community';
+import { AgGridReact } from 'ag-grid-react';
+import React from 'react';
+
+import type { PopupBaseProps } from '@/shared/types/uiTypes';
+import { AgGridEmptyComponent, numberValueFormatter } from '@aggrid';
+import { Gcol, Grow, Typo } from '@atoms';
+import { DialogBottomInfo } from '@common/DialogBottomInfo';
+import { Button } from '@uiux/Button';
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogSection,
+  DialogTitle,
+  DialogFooterArea,
+} from '@uiux/Dialog';
+import { number } from 'zod';
+import { TableFold, TableFoldBody, TableFoldHead } from '@/shared/components/common/TableFold';
+
+ModuleRegistry.registerModules([AllCommunityModule]);
+
+export const Ltpz004 = ({ open, onOpenChange }: PopupBaseProps) => {
+  type DummyDataType = {
+    id: number;
+    field1: string | number;
+    field2: string | number;
+    field3: string | number;
+  };
+
+  const DummyData: DummyDataType[] = [
+    {
+      id: 1,
+      field1: '보통약관(상해80%이상후유장해)(간편)',
+      field2: '15~80세',
+      field3: 100,
+    },
+    {
+      id: 2,
+      field1: '보통약관(상해80%이상후유장해)(간편)',
+      field2: '15~80세',
+      field3: 10000,
+    },
+    {
+      id: 3,
+      field1: '보통약관(상해80%이상후유장해)(간편)',
+      field2: '15~80세',
+      field3: 10000,
+    },
+    {
+      id: 4,
+      field1: '보통약관(상해80%이상후유장해)(간편)',
+      field2: '15~80세',
+      field3: 10000,
+    },
+    {
+      id: 5,
+      field1: '보통약관(상해80%이상후유장해)(간편)',
+      field2: '15~80세',
+      field3: 10000,
+    },
+    {
+      id: 6,
+      field1: '보통약관(상해80%이상후유장해)(간편)',
+      field2: '15~80세',
+      field3: 10000,
+    },
+    {
+      id: 7,
+      field1: '보통약관(상해80%이상후유장해)(간편)',
+      field2: '15~80세',
+      field3: 10000,
+    },
+    {
+      id: 8,
+      field1: '보통약관(상해80%이상후유장해)(간편)',
+      field2: '15~80세',
+      field3: 10000,
+    },
+    {
+      id: 9,
+      field1: '보통약관(상해80%이상후유장해)(간편)',
+      field2: '15~80세',
+      field3: 10000,  
+    },
+    {
+      id: 10,
+      field1: '보통약관(상해80%이상후유장해)(간편)',
+      field2: '15~80세',
+      field3: 10000,
+    },
+    {
+      id: 11,
+      field1: '보통약관(상해80%이상후유장해)(간편)',
+      field2: '15~80세',
+      field3: 10000,
+    },
+    {
+      id: 12,
+      field1: '보통약관(상해80%이상후유장해)(간편)',
+      field2: '15~80세',
+      field3: 10000,
+    },
+    {
+      id: 13,
+      field1: '보통약관(상해80%이상후유장해)(간편)',
+      field2: '15~80세',
+      field3: 10000,
+    },
+    {
+      id: 14,
+      field1: '보통약관(상해80%이상후유장해)(간편)',
+      field2: '15~80세',
+      field3: 10000,
+    },
+    {
+      id: 15,
+      field1: '보통약관(상해80%이상후유장해)(간편)',
+      field2: '15~80세',
+      field3: 10000,
+    },
+    {
+      id: 16,
+      field1: '보통약관(상해80%이상후유장해)(간편)',
+      field2: '15~80세',
+      field3: 10000,
+    },
+    {
+      id: 17,
+      field1: '보통약관(상해80%이상후유장해)(간편)',
+      field2: '15~80세',
+      field3: 10000,
+    },
+    {
+      id: 18,
+      field1: '보통약관(상해80%이상후유장해)(간편)',
+      field2: '15~80세',
+      field3: 10000,
+    },
+    {
+      id: 19,
+      field1: '보통약관(상해80%이상후유장해)(간편)',
+      field2: '15~80세',
+      field3: 10000,
+    },
+    {
+      id: 20,
+      field1: '보통약관(상해80%이상후유장해)(간편)',
+      field2: '15~80세',
+      field3: 10000,
+    },
+  ];
+  const [rowData] = React.useState<DummyDataType[]>(DummyData);
+
+  const columnDefs: ColDef<DummyDataType>[] = [
+    {
+      headerName: '담보명',
+      field: 'field1',
+      flex: 1,
+      cellClass: 'text-left',
+    },
+    {
+      headerName: '가입연령',
+      field: 'field2',
+      width: 86,
+      cellClass: 'text-center',
+    },
+    {
+      headerName: '가입금액(만원)',
+      field: 'field3',
+      width: 86,
+      cellClass: 'text-right ',
+      valueFormatter: numberValueFormatter, 
+    },
+  ];
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent showCloseButton resizable={true} size="md">
+        <DialogHeader>
+          <DialogTitle>
+            <Typo tag={'strong'} variant={'heading-lg'}>
+              담보보기
+            </Typo>
+            <Typo tag={'p'} variant={'body-xl'}>
+              (LTPZ004)
+            </Typo>
+          </DialogTitle>
+        </DialogHeader>
+
+        <DialogSection className="grid-rows-[1fr]">
+          <Gcol className='bg-[#EFF8FF] border border-[#CBE3FF] rounded-[0.6rem] p-[1rem]' placement='ss'>
+            <Typo tag={'strong'} variant={'body-lg'} weight={'bold'}>한화 3N5 더간편건강보험(세만기형) 2601 상품의 담보코드와 메시지입니다.</Typo>
+            <Typo tag={'p'} variant={'body-sm'} color={'gray'}>1종(납입후50%해약환급금지급형, 납입면제운영형, 3N5간편고지형Ⅲ)</Typo>
+          </Gcol>
+          <TableFold>
+            <TableFoldHead title="1형(355간편고지형)(프리미엄올인원플랜)(1.7.8.9형)(15~80세)">
+            </TableFoldHead>
+            <TableFoldBody>
+              <div className="ag-theme-alpine">
+                <AgGridReact<DummyDataType>
+                  getRowId={(params) => String(params.data.id)}
+                  rowData={rowData}
+                  columnDefs={columnDefs}
+                  noRowsOverlayComponent={AgGridEmptyComponent}
+                  defaultColDef={{
+                    sortable: true,
+                    resizable: true,
+                  }}
+                  domLayout="autoHeight"
+                />
+              </div>
+            </TableFoldBody>
+          </TableFold>
+        </DialogSection>
+
+        <DialogFooter>
+          <DialogFooterArea>
+            <Grow>
+              <Button
+                variant={'outlined'}
+                size={'xl'}
+                color={'gray-light'}
+                onClick={onOpenChange ? () => onOpenChange(false) : undefined}
+              >
+                닫기
+              </Button>
+            </Grow>
+          </DialogFooterArea>
+          <DialogBottomInfo />
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
---
+++ src/stories/popup/LTPZ004.stories.tsx
@@ -0,0 +1,24 @@
+import { Controls, Primary, Title } from '@storybook/addon-docs/blocks';
+import type { Meta, StoryObj } from '@storybook/react';
+import * as React from 'react';
+import { Ltpz004 } from '@/features/pub/components/popups/Ltpz004';
+import { LayoutDoc } from '@layout/BaseLayout';
+import { Button } from '@uiux/Button';
+
+export default {
+  title: 'popup/Ltpz004',
+  component: Ltpz004,
+};
+
+export const Default = () => {
+  const [open, setOpen] = React.useState(true);
+  return (
+    <LayoutDoc>
+      <div className='flex w-full h-screen items-center justify-center max-w-[118rem] outline outline-1 outline-[red] -outline-offset-2 mx-auto'>
+        <Button variant={'contained'} onClick={() => setOpen(true)}>Ltpz999 열기</Button>
+      </div>
+
+      <Ltpz004 open={open} onOpenChange={setOpen} />
+    </LayoutDoc>
+  );
+};
