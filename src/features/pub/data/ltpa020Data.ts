075f043ecc480b10458e2c388d76ba0f29a8bcad
new-Niel <endlessiris@netive.co.kr>
2026-04-10 20:23:21 +0900

    Z020

---
+++ src/features/pub/data/ltpa020Data.ts
@@ -0,0 +1,269 @@
+export type DummyDataType = {
+	id: number;
+	field1: string | number;
+	field2: string | number;
+	importance: boolean;
+	badge?: string[];
+	field3: string | number;
+};
+
+export const dummyData: DummyDataType[] = [
+	{
+		id: 1,
+		field1: '종합건강22',
+		field2: '한화 더 경증 간편건강보험간연만기 갱신형)2601',
+		importance: true,
+		badge: ['무해지', '할증'],
+		field3: '15~90세',
+	},
+	{
+		id: 2,
+		field1: '종합건강',
+		field2: '한화 311 간편건강보험(연만기 경신형)',
+		importance: false,
+		field3: '15~90세',
+	},
+	{
+		id: 3,
+		field1: '종합건강',
+		field2: '한화 311 간편건강보험(연만기 경신형)',
+		importance: false,
+		badge: ['간편'],
+		field3: '15~90세',
+	},
+	{
+		id: 4,
+		field1: '종합건강',
+		field2: '한화 311 간편건강보험(연만기 경신형)',
+		importance: false,
+		badge: ['할증'],
+		field3: '15~90세',
+	},
+	{
+		id: 5,
+		field1: '종합건강',
+		field2: '한화 311 간편건강보험(연만기 경신형)',
+		importance: false,
+		field3: '15~90세',
+	},
+	{
+		id: 6,
+		field1: '종합건강',
+		field2: '한화 311 간편건강보험(연만기 경신형)',
+		importance: false,
+		badge: ['여성', '무해지', '할증'],
+		field3: '15~90세',
+	},
+	{
+		id: 7,
+		field1: '종합건강',
+		field2: '한화 311 간편건강보험(연만기 경신형)',
+		importance: false,
+		field3: '15~90세',
+	},
+	{
+		id: 8,
+		field1: '종합건강',
+		field2: '한화 311 간편건강보험(연만기 경신형)',
+		importance: false,
+		field3: '15~90세',
+	},
+	{
+		id: 9,
+		field1: '종합건강',
+		field2: '한화 311 간편건강보험(연만기 경신형)',
+		importance: false,
+		field3: '15~90세',
+	},
+	{
+		id: 10,
+		field1: '종합건강',
+		field2: '한화 311 간편건강보험(연만기 경신형)',
+		importance: false,
+		field3: '15~90세',
+	},
+	{
+		id: 11,
+		field1: '종합건강',
+		field2: '한화 311 간편건강보험(연만기 경신형)',
+		importance: false,
+		field3: '15~90세',
+	},
+	{
+		id: 12,
+		field1: '종합건강',
+		field2: '한화 311 간편건강보험(연만기 경신형)',
+		importance: false,
+		field3: '15~90세',
+	},
+	{
+		id: 13,
+		field1: '종합건강',
+		field2: '한화 311 간편건강보험(연만기 경신형)',
+		importance: false,
+		field3: '15~90세',
+	},
+	{
+		id: 14,
+		field1: '종합건강',
+		field2: '한화 311 간편건강보험(연만기 경신형)',
+		importance: false,
+		field3: '15~90세',
+	},
+	{
+		id: 15,
+		field1: '종합건강',
+		field2: '한화 311 간편건강보험(연만기 경신형)',
+		importance: false,
+		field3: '15~90세',
+	},
+	{
+		id: 16,
+		field1: '종합건강',
+		field2: '한화 311 간편건강보험(연만기 경신형)',
+		importance: false,
+		field3: '15~90세',
+	},
+	{
+		id: 17,
+		field1: '종합건강',
+		field2: '한화 311 간편건강보험(연만기 경신형)',
+		importance: false,
+		field3: '15~90세',
+	},
+	{
+		id: 18,
+		field1: '종합건강',
+		field2: '한화 311 간편건강보험(연만기 경신형)',
+		importance: false,
+		field3: '15~90세',
+	},
+	{
+		id: 19,
+		field1: '종합건강',
+		field2: '한화 311 간편건강보험(연만기 경신형)',
+		importance: false,
+		field3: '15~90세',
+	},
+	{
+		id: 20,
+		field1: '종합건강',
+		field2: '한화 311 간편건강보험(연만기 경신형)',
+		importance: false,
+		field3: '15~90세',
+	},
+];
+
+export type DummyDataType2 = {
+	id: number;
+	field1: string | number;
+	field2: string | number;
+	btn?: boolean;
+};
+
+export const dummyData2: DummyDataType2[] = [
+	{
+		id: 1,
+		field1: '1종',
+		field2: '한화 3N5 더 간편건강보험(연만기 갱신형)2601',
+		btn: true,
+	},
+	{
+		id: 2,
+		field1: '2종',
+		field2: '한화 3N5 더 간편건강보험(연만기 갱신형)2601',
+	},
+	{
+		id: 3,
+		field1: '3종',
+		field2: '한화 3N5 더 간편건강보험(연만기 갱신형)2601',
+	},
+	{
+		id: 4,
+		field1: '4종',
+		field2: '한화 3N5 더 간편건강보험(연만기 갱신형)2601',
+	},
+	{
+		id: 5,
+		field1: '4종',
+		field2: '한화 3N5 더 간편건강보험(연만기 갱신형)2601',
+	},
+	{
+		id: 6,
+		field1: '4종',
+		field2: '한화 3N5 더 간편건강보험(연만기 갱신형)2601',
+	},
+	{
+		id: 7,
+		field1: '4종',
+		field2: '한화 3N5 더 간편건강보험(연만기 갱신형)2601',
+	},
+	{
+		id: 8,
+		field1: '4종',
+		field2: '한화 3N5 더 간편건강보험(연만기 갱신형)2601',
+	},
+	{
+		id: 9,
+		field1: '4종',
+		field2: '한화 3N5 더 간편건강보험(연만기 갱신형)2601',
+	},
+];
+
+export type DummyDataType3 = {
+	id: number;
+	field1: string | number;
+};
+
+export const dummyData3: DummyDataType3[] = [
+	{
+		id: 1,
+		field1: '1형(355간편고지형)(프리미엄올인원플랜)(1.718.9형)(15~80세)',
+	},
+	{
+		id: 2,
+		field1: '7형(355간편(고혈압추가고지))(프리미엄올인원플랜)(1.718.9형)(15~80세)',
+	},
+	{
+		id: 3,
+		field1: '7형(355간편(고혈압추가고지))(프리미엄올인원플랜)(1.718.9형)(15~80세)',
+	},
+	{
+		id: 4,
+		field1: '1형(355간편고지형)(프리미엄올인원플랜)(1.718.9형)(15~80세)(15~40세)',
+	},
+	{
+		id: 5,
+		field1: '1형(355간편고지형)(프리미엄올인원플랜)(1.718.9형)(15~80세)(15~1형(355간편고지형)(프리미엄올인원플랜)(1.718.9형)(15~80세))',
+	},
+	{
+		id: 6,
+		field1:
+			'1형(355간편고지형)(프리미엄올인원플랜)(1.718.9형)(15~80세)(11형(355간편고지형)(프리미엄올인원플랜)(1.718.9형)(15~80세)5~40세)',
+	},
+	{
+		id: 7,
+		field1:
+			'1형(355간편고지형)(프리미엄올인원플랜)(1.718.9형)(15~80세)(1형(355간편고지형)(프리미엄올인원플랜)(1.718.9형)(15~80세)15~40세)',
+	},
+	{
+		id: 8,
+		field1: '1형(355간편고지형)(프리미엄올인원플랜)(1.718.9형)(15~80세)(15~40세)',
+	},
+	{
+		id: 9,
+		field1: '1형(355간편고지형)(프리미엄올인원플랜)(1.718.9형)(15~80세)(15~40세)',
+	},
+	{
+		id: 10,
+		field1: '1형(355간편고지형)(프리미엄올인원플랜)(1.718.9형)(15~80세)(15~40세)',
+	},
+	{
+		id: 11,
+		field1: '1형(355간편고지형)(프리미엄올인원플랜)(1.718.9형)(15~80세)(15~40세)',
+	},
+	{
+		id: 12,
+		field1: '1형(355간편고지형)(프리미엄올인원플랜)(1.718.9형)(15~80세)(15~40세)',
+	},
+];
--- src/features/pub/sections/Ltpa020Section.tsx
+++ src/features/pub/sections/Ltpa020Section.tsx
@@ -20,6 +20,14 @@ import { AgGridEmptyComponent, createTooltipValueGetter } from '@/shared/compone
 import { useTabs } from '@/shared/hooks/useTabs';
 import { AgGridReact } from 'ag-grid-react';
 import { Badge } from '@/shared/components/uiux/Badge';
+import {
+  dummyData,
+  dummyData2,
+  dummyData3,
+  type DummyDataType,
+  type DummyDataType2,
+  type DummyDataType3,
+} from '@/features/pub/data/ltpa020Data';
 
 const productCategoryOptions = [
   { value: 'all', label: '전체' },
@@ -236,160 +244,6 @@ export default function Ltpa020Section() {
   };
   
 
-  type DummyDataType = {
-    id: number;
-    field1: string | number;
-    field2: string | number;
-    importance: boolean;
-    badge?: string[];
-    field3: string | number;
-  };
-
-  const dummyData: DummyDataType[] = [
-    {
-      id: 1,
-      field1: '종합건강22',
-      field2: '한화 더 경증 간편건강보험간연만기 갱신형)2601',
-      importance: true,
-      badge: ['무해지', '할증'],
-      field3: '15~90세',
-    },
-    {
-      id: 2,
-      field1: '종합건강',
-      field2: '한화 311 간편건강보험(연만기 경신형)',
-      importance: false,
-      field3: '15~90세',
-    },  
-    {
-      id: 3,
-      field1: '종합건강',
-      field2: '한화 311 간편건강보험(연만기 경신형)',
-      importance: false,
-      badge: ['간편'],
-      field3: '15~90세',
-    },
-    {
-      id: 4,
-      field1: '종합건강',
-      field2: '한화 311 간편건강보험(연만기 경신형)',
-      importance: false,
-      badge: ['할증'],
-      field3: '15~90세',
-    },
-    {
-      id: 5,
-      field1: '종합건강',
-      field2: '한화 311 간편건강보험(연만기 경신형)',
-      importance: false,
-      field3: '15~90세',
-    },
-    {
-      id: 6,
-      field1: '종합건강',
-      field2: '한화 311 간편건강보험(연만기 경신형)',
-      importance: false,
-      badge: ['여성','무해지', '할증'],
-      field3: '15~90세',
-    },
-    {
-      id: 7,
-      field1: '종합건강',
-      field2: '한화 311 간편건강보험(연만기 경신형)',
-      importance: false,
-      field3: '15~90세',
-    },
-    {
-      id: 8,
-      field1: '종합건강',
-      field2: '한화 311 간편건강보험(연만기 경신형)',
-      importance: false,
-      field3: '15~90세',
-    },
-    {
-      id: 9,
-      field1: '종합건강',
-      field2: '한화 311 간편건강보험(연만기 경신형)',
-      importance: false,
-      field3: '15~90세',
-    },
-    {
-      id: 10,
-      field1: '종합건강',
-      field2: '한화 311 간편건강보험(연만기 경신형)',
-      importance: false,
-      field3: '15~90세',
-    },
-    {
-      id: 11,
-      field1: '종합건강',
-      field2: '한화 311 간편건강보험(연만기 경신형)',
-      importance: false,
-      field3: '15~90세',
-    },
-    {
-      id: 12,
-      field1: '종합건강',
-      field2: '한화 311 간편건강보험(연만기 경신형)',
-      importance: false,
-      field3: '15~90세',
-    },
-    {
-      id: 13,
-      field1: '종합건강',
-      field2: '한화 311 간편건강보험(연만기 경신형)',
-      importance: false,
-      field3: '15~90세',
-    },
-    {
-      id: 14,
-      field1: '종합건강',
-      field2: '한화 311 간편건강보험(연만기 경신형)',
-      importance: false,
-      field3: '15~90세',
-    },
-    {
-      id: 15,
-      field1: '종합건강',
-      field2: '한화 311 간편건강보험(연만기 경신형)',
-      importance: false,
-      field3: '15~90세',
-    },
-    {
-      id: 16,
-      field1: '종합건강',
-      field2: '한화 311 간편건강보험(연만기 경신형)',
-      importance: false,
-      field3: '15~90세',
-    },
-    {
-      id: 17,
-      field1: '종합건강',
-      field2: '한화 311 간편건강보험(연만기 경신형)',
-      importance: false,
-      field3: '15~90세',
-    },
-    {
-      id: 18,
-      field1: '종합건강',
-      field2: '한화 311 간편건강보험(연만기 경신형)',
-      importance: false,
-      field3: '15~90세',
-    },{
-      id: 19,
-      field1: '종합건강',
-      field2: '한화 311 간편건강보험(연만기 경신형)',
-      importance: false,
-      field3: '15~90세',
-    },
-    {
-      id: 20,
-      field1: '종합건강',
-      field2: '한화 311 간편건강보험(연만기 경신형)',
-      importance: false,
-      field3: '15~90세',
-    },
-  ];
 
   const columnDefs: ColDef<DummyDataType>[] = [
     {
@@ -448,61 +302,6 @@ export default function Ltpa020Section() {
     )
   };  
 
-  type DummyDataType2 = {
-    id: number;
-    field1: string | number;
-    field2: string | number;
-    btn?: boolean;
-  };
-
-  const dummyData2: DummyDataType2[] = [
-    {
-      id: 1,
-      field1: '1종',
-      field2: '한화 3N5 더 간편건강보험(연만기 갱신형)2601',
-      btn: true
-    },
-    {
-      id: 2,
-      field1: '2종',
-      field2: '한화 3N5 더 간편건강보험(연만기 갱신형)2601',
-    },
-    {
-      id: 3,
-      field1: '3종',
-      field2: '한화 3N5 더 간편건강보험(연만기 갱신형)2601',
-    },
-    {
-      id: 4,
-      field1: '4종',
-      field2: '한화 3N5 더 간편건강보험(연만기 갱신형)2601',
-    },
-    {
-      id: 5,
-      field1: '4종',
-      field2: '한화 3N5 더 간편건강보험(연만기 갱신형)2601',
-    },
-    {
-      id: 6,
-      field1: '4종',
-      field2: '한화 3N5 더 간편건강보험(연만기 갱신형)2601',
-    },
-    {
-      id: 7,
-      field1: '4종',
-      field2: '한화 3N5 더 간편건강보험(연만기 갱신형)2601',
-    },
-    {
-      id: 8,
-      field1: '4종',
-      field2: '한화 3N5 더 간편건강보험(연만기 갱신형)2601',
-    },
-    {
-      id: 9,
-      field1: '4종',
-      field2: '한화 3N5 더 간편건강보험(연만기 갱신형)2601',
-    },
-  ];
 
   const columnDefs2: ColDef<DummyDataType2>[] = [
     {
@@ -521,62 +320,6 @@ export default function Ltpa020Section() {
   ];
   
 
-
-  type DummyDataType3 = {
-    id: number;
-    field1: string | number;
-  };
-
-  const dummyData3: DummyDataType3[] = [
-    {
-      id: 1,
-      field1: '1형(355간편고지형)(프리미엄올인원플랜)(1.718.9형)(15~80세)',
-    },
-    {
-      id: 2,
-      field1: '7형(355간편(고혈압추가고지))(프리미엄올인원플랜)(1.718.9형)(15~80세)',
-    },
-    {
-      id: 3,
-      field1: '7형(355간편(고혈압추가고지))(프리미엄올인원플랜)(1.718.9형)(15~80세)',
-    },
-    {
-      id: 4,
-      field1: '1형(355간편고지형)(프리미엄올인원플랜)(1.718.9형)(15~80세)(15~40세)',
-    },
-    {
-      id: 5,
-      field1: '1형(355간편고지형)(프리미엄올인원플랜)(1.718.9형)(15~80세)(15~1형(355간편고지형)(프리미엄올인원플랜)(1.718.9형)(15~80세))', 
-    },
-    {
-      id: 6,
-      field1: '1형(355간편고지형)(프리미엄올인원플랜)(1.718.9형)(15~80세)(11형(355간편고지형)(프리미엄올인원플랜)(1.718.9형)(15~80세)5~40세)',
-    },
-    {
-      id: 7,
-      field1: '1형(355간편고지형)(프리미엄올인원플랜)(1.718.9형)(15~80세)(1형(355간편고지형)(프리미엄올인원플랜)(1.718.9형)(15~80세)15~40세)',
-    },
-    {
-      id: 8,
-      field1: '1형(355간편고지형)(프리미엄올인원플랜)(1.718.9형)(15~80세)(15~40세)',
-    },
-    {
-      id: 9,
-      field1: '1형(355간편고지형)(프리미엄올인원플랜)(1.718.9형)(15~80세)(15~40세)',
-    },
-    {
-      id: 10,
-      field1: '1형(355간편고지형)(프리미엄올인원플랜)(1.718.9형)(15~80세)(15~40세)',
-    },
-    {
-      id: 11,
-      field1: '1형(355간편고지형)(프리미엄올인원플랜)(1.718.9형)(15~80세)(15~40세)',
-    },
-    {
-      id: 12,
-      field1: '1형(355간편고지형)(프리미엄올인원플랜)(1.718.9형)(15~80세)(15~40세)',
-    },
-  ];
 
   const columnDefs3: ColDef<DummyDataType3>[] = [
     {
