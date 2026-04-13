ba7c5ac141ebc60e11b9babe3f9d885865aff760
asever77 <johyunmin77@gmail.com>
2026-04-13 17:43:43 +0900

    수정

---
+++ src/app/pub/pages/LTPA030.tsx
@@ -0,0 +1,5 @@
+import Ltpa030Section from '@/features/pub/sections/Ltpa030Section';
+
+export default function Page() {
+  return <Ltpa030Section />;
+}
---
+++ src/stories/page/LTPA030.stories.tsx
@@ -0,0 +1,13 @@
+import LTPA030 from '@/app/pub/pages/LTPA030';
+import { LayoutDoc } from '@layout/BaseLayout';
+
+export default {
+  title: 'page/LTPA030',
+  component: LTPA030
+};
+
+export const Default = () => (
+  <LayoutDoc>
+    <LTPA030 />
+  </LayoutDoc>
+);
