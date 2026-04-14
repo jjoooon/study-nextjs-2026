6d173d0768ef2c7625e6a70ab3fd72fe1b818a8e
new-Niel <endlessiris@netive.co.kr>
2026-04-14 17:55:17 +0900

    003

---
+++ src/stories/popup/LTPZ003.stories.tsx
@@ -0,0 +1,23 @@
+
+import * as React from 'react';
+import { Ltpz003 } from '@/features/pub/components/popups/Ltpz003';
+import { LayoutDoc } from '@layout/BaseLayout';
+import { Button } from '@uiux/Button';
+
+export default {
+  title: 'popup/Ltpz003',
+  component: Ltpz003,
+};
+
+export const Default = () => {
+  const [open, setOpen] = React.useState(true);
+  return (
+    <LayoutDoc>
+      <div className='flex w-full h-screen items-center justify-center max-w-[118rem] outline outline-1 outline-[red] -outline-offset-2 mx-auto'>
+        <Button variant={'contained'} onClick={() => setOpen(true)}>다이얼로그 열기</Button>
+      </div>
+
+      <Ltpz003 open={open} onOpenChange={setOpen} />
+    </LayoutDoc>
+  );
+};
