d5caa7114a66c51c04eb5f7daf272a27f2e73339
new-Niel <endlessiris@netive.co.kr>
2026-04-21 15:44:08 +0900

    추가 팝업

---
+++ src/stories/popup/LTPA01002.stories.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { Ltpa01002 } from '@/features/pub/ispl/isplBsnsSupt/components/popups/Ltpa01001';
+import { LayoutDoc } from '@layout/BaseLayout';
+import { Button } from '@uiux/Button';
+
+export default {
+  title: 'app/ispl/isplBsnsSupt/components/popups/Ltpa01002',
+  component: Ltpa01002,
+};
+
+export const Default = () => {
+  const [open, setOpen] = React.useState(true);
+  return (
+    <LayoutDoc>
+      <div className='flex w-full h-screen items-center justify-center max-w-[118rem] outline outline-1 outline-[red] -outline-offset-2 mx-auto'>
+        <Button variant={'contained'} onClick={() => setOpen(true)}>다이얼로그 열기</Button>
+      </div>
+
+      <Ltpa01002 open={open} onOpenChange={setOpen} />
+    </LayoutDoc>
+  );
+};
