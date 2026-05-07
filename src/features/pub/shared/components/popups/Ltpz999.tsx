ec16b0955dd1891f77d65f427d578c93b401302b
asever77 <johyunmin77@gmail.com>
2026-05-07 09:21:30 +0900

    수정

--- src/features/pub/shared/components/popups/Ltpz999.tsx
+++ src/features/pub/shared/components/popups/Ltpz999.tsx
@@ -5,74 +5,32 @@ import { Gcol, Grow, Typo } from '@atoms';
 import { DialogBottomInfo } from '@common/DialogBottomInfo';
 import { Button } from '@uiux/Button';
 import { Dialog, DialogContent, DialogFooter, DialogSection, DialogFooterArea, DialogClose } from '@uiux/Dialog';
-import type { ColDef } from 'ag-grid-community';
+import { BulletItem } from '@/shared/components/common/BulletList';
 import { ErrorIcon } from '@/shared/components/icons/CommonIcons';
-import { BulletList, BulletListItem } from '@/shared/components/common/BulletList';
 
 const Ltpz999 = () => {
   return (
     <Dialog open>
-      <DialogContent showCloseButton={false} resizable={true} size="md" className="grid-rows-[1fr_auto]">
+      <DialogContent showCloseButton={false} resizable={true} size={'md'}>
         <DialogSection className="grid-rows-[1fr] pt-5 gap={5}">
           <Grow placement="ec" className="text-[var(--color-gray-70)]">
             코드 LTRE006(trandZomH110)
           </Grow>
-            <Gcol gap={2}>
-              <Grow className="w-[4.4rem] h-[4.4rem] bg-[var(--color-gray-5)] rounded-3xl">
-                <ErrorIcon />
-              </Grow>
-              <Typo variant={'body-lg'} className="font-bold">오류</Typo>
-            </Gcol>
-            <BulletList position="col" className="items-center">
-              <BulletListItem
-                before="1."
-                className="w-fit self-center whitespace-nowrap [&>div:last-child]:w-auto [&>div:last-child]:flex-none"
-                color="default"
-                size="md"
-                type="dot"
-              >
-                시스템 오류가 발생했습니다. 
-              </BulletListItem>
-              <BulletListItem
-                before="①"
-                className="w-fit self-center whitespace-nowrap [&>div:last-child]:w-auto [&>div:last-child]:flex-none"
-                color="default"
-                size="md"
-                type="dot"
-              >
-                시스템 문구가 더 길어질수 있습니다.
-              </BulletListItem>
-              <BulletListItem
-                before="㉠"
-                className="w-fit self-center whitespace-nowrap [&>div:last-child]:w-auto [&>div:last-child]:flex-none"
-                color="default"
-                onClick={() => {}}
-                size="md"
-                type="dot"
-              >
-                시스템 메시지는 기본 90자까지 이내를 권장합니다.
-              </BulletListItem>
-              <BulletListItem
-                before="㉠"
-                className="w-fit self-center whitespace-nowrap [&>div:last-child]:w-auto [&>div:last-child]:flex-none"
-                color="default"
-                onClick={() => {}}
-                size="md"
-                type="dot"
-              >
-                최대한 90자 이내로 정의 부탁드립니다.
-              </BulletListItem>
-              <BulletListItem
-                before="㉠"
-                className="w-fit self-center whitespace-nowrap [&>div:last-child]:w-auto [&>div:last-child]:flex-none"
-                color="default"
-                onClick={() => {}}
-                size="md"
-                type="dot"
-              >
-                시스템 오류가 발행했습니다.
-              </BulletListItem>
-            </BulletList>
+          <Gcol gap={2}>
+            <Grow className="w-[4.4rem] h-[4.4rem] bg-[var(--color-gray-5)] rounded-3xl">
+              <ErrorIcon />
+            </Grow>
+            <Typo variant={'body-lg'} className="font-bold">
+              오류
+            </Typo>
+          </Gcol>
+          <Gcol placement="cc">
+            <BulletItem type="dot">시스템 오류가 발생했습니다.</BulletItem>
+            <BulletItem type="dot">시스템 문구가 더 길어질수 있습니다.</BulletItem>
+            <BulletItem type="dot">시스템 메시지는 기본 90자까지 이내를 권장합니다.</BulletItem>
+            <BulletItem type="dot">최대한 90자 이내로 정의 부탁드립니다.</BulletItem>
+            <BulletItem type="dot">시스템 오류가 발행했습니다.</BulletItem>
+          </Gcol>
         </DialogSection>
 
         <DialogFooter>
