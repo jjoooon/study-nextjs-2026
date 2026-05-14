2aec95b6e47b8e6b6896fb0ba20cf96d45c04283
new-Niel <endlessiris@netive.co.kr>
2026-05-14 16:35:22 +0900

    058 추가

--- src/features/pub/ispl/crmtt/components/popups/Ltpz058.tsx
+++ src/features/pub/ispl/crmtt/components/popups/Ltpz058.tsx
@@ -4,9 +4,8 @@
 'use client';
 
 import { AgGridEmptyComponent } from '@aggrid';
-import { Grow, Typo } from '@atoms';
+import { Grid, Grow, Typo } from '@atoms';
 import { DialogBottomInfo } from '@common/DialogBottomInfo';
-import { FormCell, FormRow, FormTable } from '@common/FormTable';
 import { Button } from '@uiux/Button';
 import {
   Dialog,
@@ -19,90 +18,283 @@ import {
   DialogClose,
 } from '@uiux/Dialog';
 import { Input } from '@uiux/Input';
-import type { ColDef } from 'ag-grid-enterprise';
+import type { ColDef, ColGroupDef } from 'ag-grid-enterprise';
 import { AgGridReact } from 'ag-grid-react';
 import * as React from 'react';
 
 import '@/shared/lib/agGridPub';
+import { FormCell, FormRow, FormTable } from '@/shared/components/common/FormTable';
+import { SearchIcon } from '@/shared/components/icons/CommonIcons';
+import { RadioGroup, RadioGroupItem } from '@/shared/components/uiux/RadioGroup';
+import { Textarea } from '@/shared/components/uiux/Textarea';
+// 대분류
+type DummyDataTypeA = {
+  id: number;
+  field1: string;
+};
+
+const DummyDataA: DummyDataTypeA[] = [
+  { id: 1, field1: '주택' },
+  { id: 1, field1: '일반' },
+  { id: 2, field1: '공장' },
+];
+
+// 중분류
+type DummyDataTypeB = {
+  id: number;
+  field1: string;
+};
+
+const DummyDataB: DummyDataTypeB[] = [
+  { id: 1, field1: '근린생활시설' },
+  { id: 2, field1: '근린생활시설' },
+  { id: 3, field1: '화학공업' },
+  { id: 4, field1: '근린생활시설' },
+  { id: 5, field1: '근린생활시설' },
+  { id: 6, field1: '화학공업' },
+  { id: 7, field1: '근린생활시설' },
+  { id: 8, field1: '근린생활시설' },
+  { id: 9, field1: '화학공업' },
+  { id: 10, field1: '근린생활시설' },
+  { id: 11, field1: '근린생활시설' },
+  { id: 12, field1: '화학공업' },
+  { id: 13, field1: '근린생활시설' },
+  { id: 14, field1: '근린생활시설' },
+  { id: 15, field1: '화학공업' },
+];
+
+// 전체
+type DummyDataTypeAll = {
+  id: number;
+  field1: string;
+  field2: string;
+  field3: string;
+  field4: string;
+};
+
+const DummyDataAll: DummyDataTypeAll[] = [
+  { id: 1, field1: '일반', field2: '근린생활시설', field3: '010100', field4: '(1)휴게음식점' },
+  { id: 2, field1: '공장', field2: '근린생활시설', field3: '010200', field4: '(2)일반음식점' },
+  { id: 3, field1: '공장', field2: '화학공업', field3: '043400', field4: '의약품제조' },
+  { id: 4, field1: '일반', field2: '근린생활시설', field3: '010100', field4: '(1)휴게음식점' },
+  { id: 5, field1: '공장', field2: '근린생활시설', field3: '010200', field4: '(2)일반음식점' },
+  { id: 6, field1: '공장', field2: '화학공업', field3: '043400', field4: '의약품제조' },
+  { id: 7, field1: '일반', field2: '근린생활시설', field3: '010100', field4: '(1)휴게음식점' },
+  { id: 8, field1: '공장', field2: '근린생활시설', field3: '010200', field4: '(2)일반음식점' },
+  { id: 9, field1: '공장', field2: '화학공업', field3: '043400', field4: '의약품제조' },
+];
 
 const Ltpz058 = () => {
-  type DummyDataType = {
-    id: number;
-    field1: string;
-    field2: string;
-    field3: string;
-  };
+  type SearchCategoryType = '분류기준' | '업종명';
 
-  const DummyData: DummyDataType[] = [
-    { id: 1, field1: '최종설계저장', field2: '간편설계', field3: '수정일시:2016-03-14 김한화(12312312)' },
-    { id: 2, field1: '가입설계동의', field2: '', field3: '' },
-    { id: 3, field1: '설계심사', field2: '', field3: '' },
-    { id: 4, field1: '서류출력', field2: '미출력', field3: '' },
-    { id: 5, field1: '서류스캔', field2: '미스캔', field3: '' },
-  ];
+  const [searchCategory, setSearchCategory] = React.useState<SearchCategoryType>('분류기준');
 
-  const columnDefs: ColDef<DummyDataType>[] = [
+  const columnDefsA: ColDef<DummyDataTypeA>[] = [
     {
-      headerName: '업무구분',
+      headerName: '대분류',
       field: 'field1',
-      width: 120,
-      cellClass: 'text-center font-bold',
-    },
-    {
-      headerName: '업무상태',
-      field: 'field2',
-      width: 80,
-      cellClass: 'text-center',
-    },
-    {
-      headerName: '주요내용',
-      field: 'field3',
       flex: 1,
       cellClass: 'text-center',
     },
   ];
 
-  const [rowData] = React.useState<DummyDataType[]>(DummyData);
-  // M2. 신규 페이지
+  const columnDefsB: ColDef<DummyDataTypeB>[] = [
+    {
+      headerName: '중분류',
+      field: 'field1',
+      flex: 1,
+      cellClass: 'text-center',
+    },
+  ];
+  const columnDefsAll: (ColDef<DummyDataTypeAll> | ColGroupDef<DummyDataTypeAll>)[] = [
+    {
+      headerName: '대분류',
+      field: 'field1',
+      width: 80,
+      cellClass: 'text-center',
+    },
+    {
+      headerName: '중분류',
+      field: 'field2',
+      width: 150,
+      cellClass: 'text-center',
+    },
+    {
+      headerName: '영업위종',
+      flex: 1,
+      cellClass: 'text-center',
+      children: [
+        {
+          headerName: '',
+          field: 'field3',
+          width: 80,
+          cellClass: 'text-center justify-center',
+        },
+        {
+          headerName: '',
+          field: 'field4',
+          flex: 1,
+          cellClass: 'text-left',
+        },
+      ],
+    },
+  ];
+
+  const columnDefsBusinessTypeOnly: (ColDef<DummyDataTypeAll> | ColGroupDef<DummyDataTypeAll>)[] = [
+    {
+      headerName: '영업위종',
+      flex: 1,
+      cellClass: 'text-center',
+      children: [
+        {
+          headerName: '',
+          field: 'field3',
+          width: 80,
+          cellClass: 'text-center justify-center',
+        },
+        {
+          headerName: '',
+          field: 'field4',
+          flex: 1,
+          cellClass: 'text-left',
+        },
+      ],
+    },
+  ];
+
   return (
     <Dialog open>
-      <DialogContent showCloseButton resizable={false} size="md">
+      <DialogContent showCloseButton resizable={false} size="xl">
         <DialogHeader>
           <DialogTitle>
             <Typo tag={'strong'} variant={'heading-lg'}>
-              가입설계 이력조회
+              업종코드조회
+            </Typo>
+            <Typo tag={'p'} variant={'body-xl'}>
+              (LTPZ058)
             </Typo>
           </DialogTitle>
         </DialogHeader>
 
         <DialogSection className="grid-rows-[auto_1fr]">
-          <Grow className="w-full" variant="box-round" placement={'ss'}>
-            <FormTable caption="설계번호" variant="none" cols={['w-[1rem]', 'w-auto']}>
-              <FormRow>
-                <FormCell title={'설계번호'}>
-                  <Input value={'LA123123123123-1'} variant="info" readOnly />
-                  <Input value={'테아'} variant="info" readOnly />
-                </FormCell>
-              </FormRow>
-            </FormTable>
-          </Grow>
-
-          <div className="ag-theme-alpine">
-            <AgGridReact<DummyDataType>
-              getRowId={(params) => String(params.data.id)}
-              noRowsOverlayComponent={AgGridEmptyComponent}
-              rowData={rowData}
-              columnDefs={columnDefs}
-              defaultColDef={{
-                sortable: false,
-                resizable: false,
+          <Grow className="w-full" variant="box-round" placement={'sc'}>
+            <RadioGroup
+              value={searchCategory}
+              className="flex"
+              onValueChange={(value) => {
+                if (value === '분류기준' || value === '업종명') {
+                  setSearchCategory(value);
+                }
               }}
-              singleClickEdit={true}
-              rowClassRules={{}}
-              domLayout="autoHeight"
-              alwaysShowVerticalScroll={true}
+            >
+              {[
+                { value: '분류기준', label: '분류기준' },
+                { value: '업종명', label: '업종명' },
+              ].map((option) => (
+                <RadioGroupItem key={option.value} value={option.value}>
+                  {option.label}
+                </RadioGroupItem>
+              ))}
+            </RadioGroup>
+            <Input
+              aria-label="설계번호 검색"
+              value={''}
+              width={180}
+              onChange={() => {}}
+              readOnly={searchCategory === '분류기준'}
             />
-          </div>
+            {searchCategory !== '분류기준' && (
+              <Button aria-label="검색" variant={'outlined'} only="icon" size={'lg'} color={'gray-light'}>
+                <SearchIcon color={'var(--color-primary-50)'} />
+              </Button>
+            )}
+          </Grow>
+          {searchCategory === '분류기준' && (
+            <>
+              {/* 분류기준  */}
+              <Grid className="w-full h-full grid-cols-[1fr_2fr_3fr] gap-4">
+                <div className="ag-theme-alpine">
+                  <AgGridReact<DummyDataTypeA>
+                    getRowId={(params) => String(params.data.id)}
+                    noRowsOverlayComponent={AgGridEmptyComponent}
+                    rowData={DummyDataA}
+                    columnDefs={columnDefsA}
+                    defaultColDef={{
+                      sortable: false,
+                      resizable: false,
+                    }}
+                    singleClickEdit={true}
+                    rowClassRules={{}}
+                    domLayout="autoHeight"
+                    alwaysShowVerticalScroll={true}
+                  />
+                </div>
+                <div className="ag-theme-alpine">
+                  <AgGridReact<DummyDataTypeB>
+                    getRowId={(params) => String(params.data.id)}
+                    noRowsOverlayComponent={AgGridEmptyComponent}
+                    rowData={DummyDataB}
+                    columnDefs={columnDefsB}
+                    defaultColDef={{
+                      sortable: false,
+                      resizable: false,
+                    }}
+                    singleClickEdit={true}
+                    rowClassRules={{}}
+                    domLayout="autoHeight"
+                  />
+                </div>
+                <div className="ag-theme-alpine">
+                  <AgGridReact<DummyDataTypeAll>
+                    getRowId={(params) => String(params.data.id)}
+                    noRowsOverlayComponent={AgGridEmptyComponent}
+                    rowData={DummyDataAll}
+                    columnDefs={columnDefsBusinessTypeOnly}
+                    defaultColDef={{
+                      sortable: false,
+                      resizable: false,
+                    }}
+                    singleClickEdit={true}
+                    rowClassRules={{}}
+                    domLayout="autoHeight"
+                    groupHeaderHeight={30}
+                    headerHeight={0}
+                  />
+                </div>
+              </Grid>
+              {/* //분류기준  */}
+            </>
+          )}
+          {searchCategory === '업종명' && (
+            <Grid>
+              <div className="ag-theme-alpine">
+                <AgGridReact<DummyDataTypeAll>
+                  getRowId={(params) => String(params.data.id)}
+                  noRowsOverlayComponent={AgGridEmptyComponent}
+                  rowData={DummyDataAll}
+                  columnDefs={columnDefsAll}
+                  defaultColDef={{
+                    sortable: false,
+                    resizable: false,
+                  }}
+                  singleClickEdit={true}
+                  rowClassRules={{}}
+                  domLayout="autoHeight"
+                  groupHeaderHeight={30}
+                  headerHeight={0}
+                />
+              </div>
+            </Grid>
+          )}
+          <FormTable caption="업종설명" cols={['w-[10rem]', 'w-auto']} lineTop variant="default">
+            <FormRow>
+              <FormCell title={'업종설명'}>
+                <Grow className="w-full [&>div]:w-full!" placement="ss">
+                  <Textarea placeholder="내용을 입력하세요" showMinLengthCount />
+                </Grow>
+              </FormCell>
+            </FormRow>
+          </FormTable>
         </DialogSection>
 
         <DialogFooter>
