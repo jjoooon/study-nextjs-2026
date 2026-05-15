4221fce219e9a1cf21f975000f0c6dbfd09bc75c
허승하 <hide5075@gmail.com>
2026-05-15 18:10:20 +0900

    보험신용정보 통합조회 작업중

---
+++ src/app/pub/ispl/pages/LTPA240.tsx
@@ -0,0 +1,8 @@
+/*
+ * COPYRIGHT (c) 2026 All rights reserved by HANWHA General Insurance.
+ */
+import Ltpa240Section from '@/features/pub/ispl/isplBsnsSupt/sections/Ltpa240Section';
+
+export default function Page() {
+  return <Ltpa240Section />;
+}
