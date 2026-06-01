31c52c4db01585df1321694c0039b3975fa39dca
허승하 <hide5075@gmail.com>
2026-06-01 13:52:28 +0900

    agGrid width 수정

--- src/features/pub/ispl/isplBsnsSupt/components/popups/Ltpz080.tsx
+++ src/features/pub/ispl/isplBsnsSupt/components/popups/Ltpz080.tsx
@@ -80,25 +80,25 @@ const Ltpz080 = () => {
     {
       headerName: '담보코드',
       field: 'field1',
-      width: 140,
+      width: 90,
       cellClass: 'text-center',
     },
     {
       headerName: '담보명',
       field: 'field2',
-      flex: 1,
+      flex: 8,
       tooltipValueGetter: createTooltipValueGetter<DummyData1Type>({ field: 'field2' }),
     },
     {
       headerName: '담보그룹',
       field: 'field3',
-      width: 140,
+      width: 110,
       cellClass: 'text-center',
     },
     {
       headerName: '예외',
       field: 'field4',
-      width: 140,
+      flex: 1,
       cellClass: 'text-center',
     },
   ];
@@ -187,7 +187,7 @@ const Ltpz080 = () => {
                 columnDefs={columnDefs1}
                 defaultColDef={{
                   sortable: true,
-                  resizable: false,
+                  resizable: true, // 2026-06-01 true로 변경
                 }}
                 singleClickEdit={true}
                 domLayout="normal"
