3b8c2b1a748d32d167648e1741ab474cf56c0506
윤준모 <yjm5848@naver.com>
2026-06-08 15:47:55 +0900

    파일 초깃값 전달

--- src/features/sample/popuptest/sections/PopupTestSection.tsx
+++ src/features/sample/popuptest/sections/PopupTestSection.tsx
@@ -7,17 +7,47 @@ import { open } from '@/shared/utils/popup/popupApi';
 
 const logger = log.getLogger('Popup');
 
+const filesForPopup = [
+  {
+    fileExtension: 'png',
+    fileSize: 6770,
+    fileType: 'image/png',
+    filename: 'file1',
+    id: '000',
+  },
+  {
+    fileExtension: 'png',
+    fileSize: 5000,
+    fileType: 'image/png',
+    filename: 'file2',
+    id: '111',
+  },
+];
+
+const filesForButton = [
+  {
+    ext: 'png',
+    name: 'file1',
+    key: '000',
+  },
+  {
+    ext: 'png',
+    name: 'file2',
+    key: '111',
+  },
+];
+
 export default function PopupTestSection() {
   useMounted(() => {
     logger.info('PopupTestSection mounted');
   });
 
   const handleClickButton = async () => {
-    const result = await open('LTPZ995');
+    const result = await open('LTPZ995', { files: filesForPopup });
     logger.debug(result);
   };
 
   const handleRemove = () => {};
 
-  return <FileUpload files={[]} onClickButton={handleClickButton} onRemove={handleRemove} />;
+  return <FileUpload files={filesForButton} onClickButton={handleClickButton} onRemove={handleRemove} />;
 }
