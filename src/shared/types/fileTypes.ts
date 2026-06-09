0201dea1d331edf9305ac50854212f82bcd49ac3
윤준모 <yjm5848@naver.com>
2026-06-09 10:17:45 +0900

    주석 추가

--- src/shared/types/fileTypes.ts
+++ src/shared/types/fileTypes.ts
@@ -1,6 +1,10 @@
 /*
  * COPYRIGHT (c) 2026 All rights reserved by HANWHA General Insurance.
  */
+// TODO: @YunJunmo B/E 결정 후 타입 확정
+/**
+ * 공통 파일업로드 아이템
+ */
 export interface FileItem {
   id: string;
   filename: string;
