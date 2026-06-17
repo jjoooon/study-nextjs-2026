36c5bc1f8cab5207fca5b6acfb26fc951c3708a0
윤준모 <yjm5848@naver.com>
2026-06-17 16:10:11 +0900

    .

--- src/features/sample/popuptest/sections/PopupTestSection.tsx
+++ src/features/sample/popuptest/sections/PopupTestSection.tsx
@@ -9,14 +9,20 @@ import log from '@/shared/utils/logger';
 const logger = log.getLogger('Popup');
 
 const initialFiles: UploadFileItem[] = [
-  // {
-  //   storedFilename: 'file1',
-  //   edmsId: '000',
-  // },
-  // {
-  //   storedFilename: 'file2',
-  //   edmsId: '111',
-  // },
+  {
+    originalFilename: 'aaa.jpg',
+    fileSize: 500,
+    extension: 'jpg',
+    edmsId: '000',
+    storedFilename: 'file1',
+  },
+  {
+    originalFilename: 'bbb.jpg',
+    fileSize: 1000,
+    extension: 'jpg',
+    edmsId: '111',
+    storedFilename: 'file2',
+  },
 ];
 
 export default function PopupTestSection() {
