89e4cfd344bf8b12135061e7f2096582861dcd51
new-Niel <endlessiris@netive.co.kr>
2026-06-26 14:20:57 +0900

    110,111 진행중

---
+++ src/features/pub/shared/components/popups/Ltpz110.tsx
@@ -0,0 +1,125 @@
+/*
+ * COPYRIGHT (c) 2026 All rights reserved by HANWHA General Insurance.
+ */
+'use client';
+
+import { BulletItem } from '@/shared/components/common/BulletList';
+import { DialogBottomInfo } from '@/shared/components/common/DialogBottomInfo';
+import '@/shared/lib/agGridPub';
+import { Gcol, Grow, Typo } from '@atoms';
+
+import { FormCell, FormRow, FormTable } from '@common/FormTable';
+import { Button } from '@uiux/Button';
+import { CheckboxGroup, CheckboxGroupItem } from '@uiux/Checkbox';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogFooterArea,
+  DialogHeader,
+  DialogSection,
+  DialogTitle,
+} from '@uiux/Dialog';
+import { RadioGroup, RadioGroupItem } from '@uiux/RadioGroup';
+
+const Ltpz110 = () => {
+  return (
+    <Dialog open>
+      <DialogContent showCloseButton resizable={true} size="md">
+        <DialogHeader>
+          <DialogTitle>
+            <Typo tag={'strong'} variant={'heading-lg'}>
+              고지유형 찾기 정보 변경
+            </Typo>
+            <Typo tag={'p'} variant={'body-xl'}>
+              (Ltpz110)
+            </Typo>
+          </DialogTitle>
+        </DialogHeader>
+
+        <DialogSection>
+          <Gcol placement="ss">
+            <Typo>아래 정보를 변경 후 [재조회]를 눌러주세요.</Typo>
+            <FormTable caption="추가고지 및 적용담보 설정" cols={['w-[7rem]', 'w-[7rem]', 'w-auto']}>
+              <FormRow>
+                <FormCell title="추가고지" titleRowSpan={2} tdNone />
+                <FormCell title="고혈압">
+                  <RadioGroup defaultValue="Y">
+                    <RadioGroupItem value="Y" id="hypertension-Y">
+                      있음
+                    </RadioGroupItem>
+                    <RadioGroupItem value="N" id="hypertension-N">
+                      없음
+                    </RadioGroupItem>
+                  </RadioGroup>
+                </FormCell>
+              </FormRow>
+              <FormRow>
+                <FormCell title="당뇨">
+                  <RadioGroup defaultValue="N">
+                    <RadioGroupItem value="Y" id="diabetes-Y">
+                      있음
+                    </RadioGroupItem>
+                    <RadioGroupItem value="N" id="diabetes-N">
+                      없음
+                    </RadioGroupItem>
+                  </RadioGroup>
+                </FormCell>
+              </FormRow>
+              <FormRow>
+                <FormCell title="적용담보" titleColSpan={2}>
+                  {/* <Grid className="grid-cols-4 gap-y-2">
+                    <Checkbox id="cov-1">질병휴유</Checkbox>
+                    <Checkbox id="cov-2">암</Checkbox>
+                    <Checkbox id="cov-3">2대</Checkbox>
+                    <Checkbox id="cov-4">질병입원비</Checkbox>
+                    <Checkbox id="cov-5">질병수술비</Checkbox>
+                    <Checkbox id="cov-6">상해입원비</Checkbox>
+                    <Checkbox id="cov-7">상해수술비</Checkbox>
+                  </Grid> */}
+                  <CheckboxGroup className="grid grid-cols-4 gap-y-2 gap-x-1">
+                    {[
+                      { label: '질병휴유', value: '0' },
+                      { label: '암', value: '1' },
+                      { label: '2대', value: '2' },
+                      { label: '질병입원비', value: '3' },
+                      { label: '질병수술비', value: '4' },
+                      { label: '상해입원비', value: '5' },
+                      { label: '상해수술비', value: '6' },
+                    ].map((category) => (
+                      <CheckboxGroupItem key={category.value} value={category.value}>
+                        {category.label}
+                      </CheckboxGroupItem>
+                    ))}
+                  </CheckboxGroup>
+                </FormCell>
+              </FormRow>
+            </FormTable>
+            <BulletItem type="star">
+              간편고지 정보 변경 사항은 저장되지 않으므로, 알릴사항 입력 시 새로 입력하시기 바랍니다.
+            </BulletItem>
+          </Gcol>
+        </DialogSection>
+
+        <DialogFooter>
+          <DialogFooterArea>
+            <Grow>
+              <Button variant={'contained'} size={'xl'} color={'primary'}>
+                재조회
+              </Button>
+              <DialogClose asChild>
+                <Button variant={'outlined'} size={'xl'} color={'gray-light'}>
+                  닫기
+                </Button>
+              </DialogClose>
+            </Grow>
+          </DialogFooterArea>
+          <DialogBottomInfo />
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default Ltpz110;
---
+++ src/stories/popup/LTPZ111.stories.tsx
@@ -0,0 +1,18 @@
+/*
+ * COPYRIGHT (c) 2026 All rights reserved by HANWHA General Insurance.
+ */
+import Ltpz111 from '@/features/pub/shared/components/popups/Ltpz111';
+import { LayoutDoc } from '@layout/BaseLayout';
+
+export default {
+  title: 'app/shared/components/popups/Ltpz111',
+  component: Ltpz111,
+};
+
+export const Default = () => {
+  return (
+    <LayoutDoc>
+      <Ltpz111 />
+    </LayoutDoc>
+  );
+};
