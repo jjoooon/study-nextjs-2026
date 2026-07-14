3724d55393df9fbd610f2a50f875ac150df36db2
new-Niel <endlessiris@netive.co.kr>
2026-07-14 10:56:18 +0900

    스켈레톤

---
+++ src/app/pub/ispl/pages/LTPA020b.tsx
@@ -0,0 +1,8 @@
+/*
+ * COPYRIGHT (c) 2026 All rights reserved by HANWHA General Insurance.
+ */
+import Ltpa020SectionB from '@/features/pub/ispl/gdPlSlc/sections/Ltpa020SectionB';
+
+export default function Page() {
+  return <Ltpa020SectionB />;
+}
