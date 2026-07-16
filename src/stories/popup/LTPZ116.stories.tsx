3cafd56abcf35748c3591d49d17c4a18e4e6eaac
new-Niel <endlessiris@netive.co.kr>
2026-07-16 10:38:58 +0900

    116 완료

---
+++ src/stories/popup/LTPZ116.stories.tsx
@@ -0,0 +1,18 @@
+/*
+ * COPYRIGHT (c) 2026 All rights reserved by HANWHA General Insurance.
+ */
+import Ltpz116 from '@/features/pub/shared/components/popups/Ltpz116';
+import { LayoutDoc } from '@layout/BaseLayout';
+
+export default {
+  title: 'app/shared/components/popups/Ltpz116',
+  component: Ltpz116,
+};
+
+export const Default = () => {
+  return (
+    <LayoutDoc>
+      <Ltpz116 />
+    </LayoutDoc>
+  );
+};
