93249526ef2f11735bb8ad319f61e9b09ed3b400
asever77 <johyunmin77@gmail.com>
2026-07-20 09:03:24 +0900

    005 최대높이 지정

--- src/features/pub/shared/components/popups/Ltpz005.tsx
+++ src/features/pub/shared/components/popups/Ltpz005.tsx
@@ -63,7 +63,7 @@ const Ltpz005 = ({ open = false, onOpenChange, initialActiveTab = 'common' }: Lt
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent showCloseButton resizable={true} size="2xl" className="h-full">
+      <DialogContent showCloseButton resizable={true} size="2xl" className="h-full max-h-[97rem]!">
         <DialogHeader>
           <DialogTitle>
             <Typo tag={'strong'} variant={'heading-lg'}>
