1d111d09aa8cc1f18b6e34c4734306f10e6ddd1a
new-Niel <endlessiris@netive.co.kr>
2026-07-27 14:30:52 +0900

    49까지 완료

--- src/features/pub/ispl/crmtt/components/popups/Ltpz058.tsx
+++ src/features/pub/ispl/crmtt/components/popups/Ltpz058.tsx
@@ -173,7 +173,7 @@ const Ltpz058 = () => {
 
   return (
     <Dialog open>
-      <DialogContent showCloseButton resizable={true} size="md">
+      <DialogContent showCloseButton resizable={true} size="md" className="ltpz058-popup">
         <DialogHeader>
           <DialogTitle>
             <Typo tag={'strong'} variant={'heading-lg'}>
