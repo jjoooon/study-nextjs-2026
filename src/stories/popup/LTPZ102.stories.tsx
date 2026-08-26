84f647ac77a37954ab36d28508ca1fba624db649
asever77 <johyunmin77@gmail.com>
2026-08-26 11:00:35 +0900

    102 tnwjd

--- src/stories/popup/LTPZ102.stories.tsx
+++ src/stories/popup/LTPZ102.stories.tsx
@@ -5,7 +5,7 @@ import Ltpz102 from '@/features/pub/ispl/isplBsnsSupt/components/popups/Ltpz102'
 import { LayoutDoc } from '@layout/BaseLayout';
 
 export default {
-  title: 'app/_excluded/popup/LTPZ102',
+  title: 'app/popup/LTPZ102',
   component: Ltpz102,
 };
 
