4a50b820e76a320be1e650ea1840c5bcc6a0cd4f
asever77 <johyunmin77@gmail.com>
2026-09-08 10:32:48 +0900

    수정

--- src/stories/popup/LTPZ204.stories.tsx
+++ src/stories/popup/LTPZ204.stories.tsx
@@ -1,7 +1,7 @@
 /*
  * COPYRIGHT (c) 2026 All rights reserved by HANWHA General Insurance.
  */
-import Ltpz204 from '@/features/pub/ispl/isplBsnsSupt/components/popups/Ltpz204';
+import Ltpz204 from '@/features/pub/ispl/gdPlSlc/components/popups/Ltpz204';
 import { LayoutDoc } from '@layout/BaseLayout';
 
 export default {
