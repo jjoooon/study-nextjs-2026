930402365f0657198f7d3bb3fbcc1a89199e3555
asever77 <johyunmin77@gmail.com>
2026-09-11 13:46:46 +0900

    수정

--- src/features/pub/ispl/cvrPl/components/Ltpa35002.tsx
+++ src/features/pub/ispl/cvrPl/components/Ltpa35002.tsx
@@ -116,7 +116,7 @@ export function Ltpa35002({ onIsWidthExpandedChange }: Ltpa35002Props) {
               data={Tabs}
               active={TabActive}
               setActive={TabSetActive}
-              visibleCount={6}
+              visibleCount={3}
               error={false}
               errorMsg="입력하세요."
               getValue={(tab) => String(tab.id)}
